a58eae4c5a5ab6525dd330c9c4647e75bfd2bdf0
ArulBHGE <101313097+ArulBHGE@users.noreply.github.com>
2022-03-11 16:24:33 +0530

    Add files via upload

---
+++ assignment1.2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 16:17:35 2022
+
+@author: 105042833
+"""
+
+# word reversal
+x=input("enter text to be reversed")
+print("Entered text is:",x)
+print("reversed text is :", x[::-1])
