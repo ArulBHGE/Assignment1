82bb9dda8841656e4ecabe402446a80189a31198
ArulBHGE <101313097+ArulBHGE@users.noreply.github.com>
2022-03-12 07:01:25 +0530

    Update assignment1.3.py

--- assignment1.3.py
+++ assignment1.3.py
@@ -12,7 +12,7 @@ n=int(input("enter the number of elements:"))
 for i in range(0,n):
     ele=int(input())
     lst_1.append(ele)
-print("the given list to be squared is:", lst_1)
+print("the given list is:", lst_1)
 for num in lst_1:
     if num%2==0:
         even_count+=1
